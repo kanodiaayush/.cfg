a6a194b4e21f442a10b86f79fef92f4aa15a8737
Ayush Kanodia <kanodiaayush@gmail.com>
2019-04-26 00:56:37 -0700

    Minor

--- install_config.sh
+++ install_config.sh
@@ -1,4 +1,5 @@
 # Save this script as install.sh and run it on a new machine. Do not save it as install_config.sh as that will lead to a conflict. Once installed, delete it. Or, just wget it from git hub and pipe the output to /bin/bash instead of actually saving it (curl -Llks <github raw URL> | /bin/bash)
+# Saves existing dotfiles to .config-backup
 
 git clone --bare  https://github.com/kanodiaayush/.cfg.git $HOME/.cfg
 
